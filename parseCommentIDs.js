12c146c94e87c3b220bb17f0b0dddb7ebb7b8899
JC <jeremychampt@gmail.com>
2023-08-31 18:17:14 -0400

    Function created to pull ID's from comment tree.

---
+++ parseCommentIDs.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const filePath = 'scraped.json';
+const regexPattern = /"in[0-9a-z]{5}"/g;
+
+const matchedResults = extractMatchesFromFile(filePath, regexPattern);
+console.log('Matched Results:', matchedResults);
+
+function extractMatchesFromFile(filePath, regexPattern) {
+  try {
+    const data = fs.readFileSync(filePath, 'utf8');
+    const matches = data.match(new RegExp(regexPattern, 'g'));
+    return matches || [];
+  } catch (error) {
+    console.error('Error reading or processing the file:', error.message);
+    return [];
+  }
+}
