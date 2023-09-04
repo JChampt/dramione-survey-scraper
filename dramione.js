d675981c8ace138b741064f53df1a264f36b9c8f
JC <jeremychampt@gmail.com>
2023-09-04 18:36:22 -0400

    Getting full comments via api

--- dramione.js
+++ dramione.js
@@ -1,34 +1,38 @@
-// First you need to manually expand all of the comments on the page so they are all loaded in the browser.
-// Then this will grab the node list and store it in a variable
-// NOTE: This selector only works in Chrome!!
-// let selected = document.querySelectorAll('shreddit-comment');
+const snoowrap = require('snoowrap');
+const fs = require('fs');
 
-// creates a link at the bottom of the page to save whatever content you pass to it to file.
-function saveToFile(content, fileName) {
-  const blob = new Blob([content], { type: 'text/plain' });
-
-  const a = document.createElement('a');
-  a.href = URL.createObjectURL(blob);
-  a.download = fileName;
-  a.textContent = 'Download ' + fileName;
-
-  document.body.appendChild(a);
-}
+const thread = 'x60ake'; // 2022 Megathread ID
 
 // Extracting every comment on a thread using API wrapper snoowrap
-// r.getSubmission('4j8p6d').expandReplies({limit: Infinity, depth: Infinity}).then(console.log)
+function getComments(thread, depth = 2) {
+  const OAuth = fs.readFileSync('./OAuth.json', 'utf8');
+  const r = new snoowrap(JSON.parse(OAuth));
 
-const snoowrap = require('snoowrap');
+  r.config({
+    requestDelay: 300,
+    continueAfterRatelimitError: true,
+  });
 
-// Create a new snoowrap requester with OAuth credentials.
-// For more information on getting credentials, see here: https://github.com/not-an-aardvark/reddit-oauth-helper
-const r = new snoowrap({
-  userAgent: 'put your user-agent string here',
-  clientId: 'put your client id here',
-  clientSecret: 'put your client secret here',
-  refreshToken: 'put your refresh token here',
-});
+  return r
+    .getSubmission(thread)
+    .expandReplies({ limit: Infinity, depth: depth });
+}
 
-r.getSubmission('163lqu4')
-  .expandReplies({ limit: Infinity, depth: Infinity })
-  .then(console.log);
+let json;
+
+function run(d = 2) {
+  getComments(thread, d).then((res) => {
+    json = res.toJSON();
+  });
+}
+
+function getCount(j) {
+  let count = 0;
+  const comments = j.comments;
+
+  for (let i = 0; i < comments.length; i++) {
+    count += comments[i].replies.length;
+  }
+
+  console.log(count);
+}
