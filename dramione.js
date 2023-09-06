ef44c4e4c347e0e27034815e32fa9652256728a0
JC <jeremychampt@gmail.com>
2023-09-06 08:11:37 -0400

    cleaned up variable names

--- dramione.js
+++ dramione.js
@@ -4,15 +4,15 @@ const fs = require('fs');
 const megaThread2022ID = 'x60ake';
 const megaThread2023ID = '163lqu4';
 
-let commentsJSON;
-getComments(megaThread2023ID, 2);
+let thread;
+getThread(megaThread2023ID, 2);
 
 // let [title, , author, , link] = commentsJSON.comments[2].replies[0].body.split('\n');
 // deconstruct the body of a reply.  I can't address the commments json until it is done fetching via API though, so I probably need to put
 // a delay here that checks to see if data has been gotten yet, and if not to wait half a second and try again.
 
 // Extracting every comment on a thread using API wrapper snoowrap
-function getComments(submissionID, depth = Infinity) {
+function getThread(threadID, depth = Infinity) {
   const OAuth = fs.readFileSync('./OAuth.json', 'utf8');
   const r = new snoowrap(JSON.parse(OAuth));
 
@@ -21,28 +21,25 @@ function getComments(submissionID, depth = Infinity) {
     continueAfterRatelimitError: true,
   });
 
-  r.getSubmission(submissionID)
+  r.getSubmission(threadID)
     .expandReplies({ limit: Infinity, depth: depth })
     .then((res) => {
-      commentsJSON = res.toJSON();
+      thread = res.toJSON();
     });
 }
 
-const sampleCSV = 'a,b,c,\nd,e,f';
-function writeToFile(text) {
-  fs.writeFileSync('./output.csv', text);
-}
+function printBody(thread) {
+  const comments = thread.comments;
 
-function printBody(threadJson) {
-  for (let i = 0; i < threadJson.comments.length; i++) {
-    const comment = threadJson.comments[i];
+  for (let i = 0; i < comments.length; i++) {
+    const comment = comments[i];
     console.log(comment.body);
   }
 }
 
-function countComments(json) {
+function countComments(thread) {
+  const comments = thread.comments;
   let count = 0;
-  const comments = json.comments;
 
   for (let i = 0; i < comments.length; i++) {
     count += comments[i].replies.length;
